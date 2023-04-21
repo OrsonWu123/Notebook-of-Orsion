297adcbc34504ac9cabeaff76e99f60ee42e3640
Orson Wu <72728001+OrsonWu123@users.noreply.github.com>
2023-04-21 17:21:14 +0800

    Update 1.inchs_to_centimetres.c

--- 1.inchs_to_centimetres.c
+++ 1.inchs_to_centimetres.c
@@ -6,7 +6,7 @@ int main(void){
 
      printf("请输入英寸数以换算：\n");
      scanf("%f",&inch);
-     centimetre=inch/2.54;
+     centimetre=inch*2.54;
      printf("%f英寸是%f厘米。\n",inch,centimetre);
 
      return 0;
